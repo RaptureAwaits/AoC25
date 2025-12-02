c1597fe19e2746dc0f4740f86771915a7399c036
Frederic Smythe <fredericsmythe512@gmail.com>
2025-12-02 07:14:54 +0000

    02: Part 1 completed

---
+++ aoc-02-1/src/main.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::path::Path;
+use aoc_shared::read_delimited_file;
+
+fn main() {
+    let root_dir = env::current_dir().unwrap();
+    let input_file = Path::new("inputs.txt");
+    let filepath = root_dir.join(input_file);
+
+    let mut invalid_id_sum: u64 = 0;
+
+    for input in read_delimited_file(&filepath, ',') {
+        let range = input.split('-')
+            .map(|n| n.parse::<u64>().unwrap())
+            .collect::<Vec<u64>>();
+        let start = range[0];
+        let end = range[1] + 1;
+
+        for id in start..end {
+            if is_id_invalid(id) {
+                invalid_id_sum += id;
+                // println!("Invalid ID: {}", id)
+            }
+        }
+    }
+    println!("The sum of invalid IDs is {}", invalid_id_sum);
+}
+
+fn is_id_invalid(id: u64) -> bool {
+    let id_str = id.to_string();
+    let l = id_str.len();
+
+    if l % 2 == 1 {
+        return false;
+    }
+
+    let first = &id_str[0..l/2];
+    let second = &id_str[l/2..];
+
+    if first == second {
+        return true;
+    }
+
+    false
+}
--- aoc_shared/src/lib.rs
+++ aoc_shared/src/lib.rs
@@ -1,7 +1,13 @@
-use std::fs::File;
+use std::fs::{File, read_to_string};
 use std::io;
 use std::io::{BufRead, BufReader, Lines};
 use std::path::Path;
+
+pub fn read_delimited_file(filename: &Path, delimiter: char) -> Vec<String>{
+    let list_string = read_to_string(filename).unwrap();
+    let list = list_string.split(delimiter).map(String::from).collect::<Vec<String>>();
+    list
+}
 
 pub fn line_iterator(filename: &Path) -> io::Result<Lines<BufReader<File>>> {
     let file = File::open(filename)?;
