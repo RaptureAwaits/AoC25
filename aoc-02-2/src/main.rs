6e80fc0e595496eee8dac4acbf3cad2e57773a29
Frederic Smythe <fredericsmythe512@gmail.com>
2025-12-02 07:56:19 +0000

    02: Part 2 completed

---
+++ aoc-02-2/src/main.rs
@@ -0,0 +1,48 @@
+use std::env;
+use std::path::Path;
+use aoc_shared::read_delimited_file;
+
+fn main() {
+    let root_dir = env::current_dir().unwrap();
+    let input_file = Path::new("inputs.txt");
+    let filepath = root_dir.join(input_file);
+
+    let mut invalid_id_sum: u64 = 0;
+
+    for input in read_delimited_file(&filepath, ',') {
+        let range = input.split('-')
+            .map(|n| n.parse::<u64>().unwrap())
+            .collect::<Vec<u64>>();
+        let start = range[0];
+        let end = range[1] + 1;
+
+        for id in start..end {
+            if is_id_invalid(id) {
+                invalid_id_sum += id;
+                // println!("Invalid ID: {}", id)
+            }
+        }
+    }
+    println!("The sum of invalid IDs is {}", invalid_id_sum);
+}
+
+fn is_id_invalid(id: u64) -> bool {
+    let id_str = id.to_string();
+    let l = id_str.chars().count() as u8;
+
+    let l_max = {l as f32 / 2.0}.floor() as u8 + 1;
+
+    for substr_len in 1..l_max {
+        if l % substr_len != 0 {
+            continue;
+        }
+
+        let substr = &id_str[0..substr_len as usize];
+        let reps = l / substr_len;
+        if reps > 1 && substr.repeat(reps as usize) == id_str {
+            return true;
+        }
+    }
+
+    false
+}
