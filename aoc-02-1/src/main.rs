3c46a7bc070e8241f4af27368e0fc97c6cb6c574
Frederic Smythe <fredericsmythe512@gmail.com>
2025-12-03 05:12:19 +0000

    02: Packaged day 2 into a single crate

--- aoc-02-1/src/main.rs
+++ aoc-02-1/src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     let input_file = Path::new("inputs.txt");
     let filepath = root_dir.join(input_file);
 
-    let mut invalid_id_sum: u64 = 0;
+    let mut invalid_id_sum_p1: u64 = 0;
+    let mut invalid_id_sum_p2: u64 = 0;
 
     for input in read_delimited_file(&filepath, ',') {
         let range = input.split('-')
@@ -17,16 +18,22 @@ fn main() {
         let end = range[1] + 1;
 
         for id in start..end {
-            if is_id_invalid(id) {
-                invalid_id_sum += id;
+            if is_id_invalid_p1(id) {
+                invalid_id_sum_p1 += id;
+                // println!("Invalid ID: {}", id)
+            }
+
+            if is_id_invalid_p2(id) {
+                invalid_id_sum_p2 += id;
                 // println!("Invalid ID: {}", id)
             }
         }
     }
-    println!("The sum of invalid IDs is {}", invalid_id_sum);
+    println!("(Part 1) The sum of invalid IDs is {}", invalid_id_sum_p1);
+    println!("(Part 2) The sum of invalid IDs is {}", invalid_id_sum_p2);
 }
 
-fn is_id_invalid(id: u64) -> bool {
+fn is_id_invalid_p1(id: u64) -> bool {
     let id_str = id.to_string();
     let l = id_str.chars().count();
 
@@ -39,6 +46,27 @@ fn is_id_invalid(id: u64) -> bool {
 
     if first == second {
         return true;
+    }
+
+    false
+}
+
+fn is_id_invalid_p2(id: u64) -> bool {
+    let id_str = id.to_string();
+    let l = id_str.chars().count() as u8;
+
+    let l_max = {l as f32 / 2.0}.floor() as u8 + 1;
+
+    for substr_len in 1..l_max {
+        if l % substr_len != 0 {
+            continue;
+        }
+
+        let substr = &id_str[0..substr_len as usize];
+        let reps = l / substr_len;
+        if reps > 1 && substr.repeat(reps as usize) == id_str {
+            return true;
+        }
     }
 
     false
