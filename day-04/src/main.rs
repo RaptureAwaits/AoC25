f04a5bffc44f05253990d2d4662364b436b84941
Frederic Smythe <fredericsmythe512@gmail.com>
2025-12-04 09:30:01 +0000

    04: Part 2 completed

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -6,10 +6,12 @@ fn main() {
     let mut row_window: Vec<Vec<char>> = vec![vec![]];
     let mut row_len: usize = 0;
 
-    let mut accessible_papers: u32 = 0;
+
+    let mut p2_tiles: Vec<Vec<char>> = vec![vec![]];
+
+    // This block completes part 1 whilst completing one iteration for part 2
+    let mut removed_papers: u32 = 0;
     if let Ok(rows) = line_iterator(&filepath) {
-
-
         for row in rows.map_while(Result::ok) {
 
             let mut tiles: Vec<char> = row.chars().collect::<Vec<char>>();
@@ -19,32 +21,56 @@ fn main() {
 
             if row_window.len() == 1 {
                 row_window.push(vec!['-'; row_len + 2]);
+                p2_tiles.push(vec!['-'; row_len + 2]);
                 row_window.push(tiles);
                 continue;
             }
 
-            // Shift the window down a row
-            row_window.remove(0);
-            row_window.push(tiles);
-            accessible_papers += count_middle_row(&row_window);
+            removed_papers += process_row_window(&mut row_window, tiles, &mut p2_tiles);
         }
 
         // Push one more "empty" row to count the last line of input
-        row_window.remove(0);
-        row_window.push(vec!['-'; row_len + 2]);
-        accessible_papers += count_middle_row(&row_window);
+        removed_papers += process_row_window(&mut row_window, vec!['-'; row_len + 2], &mut p2_tiles);
+        p2_tiles.push(vec!['-'; row_len + 2]);
     }
-    println!("The total number of accessible papers is {}", accessible_papers);
+    println!("The total number of accessible papers before removal is {}", removed_papers);
+
+    let mut removed_papers_this_iter: u32 = 1;
+    while removed_papers_this_iter > 0 {
+        let mut row_window: Vec<Vec<char>> = p2_tiles[0..3].to_vec();
+        let mut new_tiles: Vec<Vec<char>> = vec![vec![], vec!['-'; row_len + 2]];
+
+        removed_papers_this_iter = 0;
+        for row_index in 1..p2_tiles.len() - 2 {
+            removed_papers_this_iter += process_row_window(&mut row_window, p2_tiles[row_index + 2].to_vec(), &mut new_tiles)
+        }
+        new_tiles.push(vec!['-'; row_len + 2]);
+        p2_tiles = new_tiles;
+        removed_papers += removed_papers_this_iter;
+        // _display_tile_grid(&p2_tiles);
+        // _ = stdin().read_line(&mut String::new()).unwrap();
+    }
+    println!("The total number of removable papers is {}", removed_papers);
 }
 
-fn count_middle_row(row_window: &Vec<Vec<char>>) -> u32 {
+fn process_row_window(row_window: &mut Vec<Vec<char>>, new_row: Vec<char>, new_tile_grid: &mut Vec<Vec<char>>) -> u32 {
+    row_window.remove(0);
+    row_window.push(new_row);
+
+    let (removed_papers, new_tiles) = count_middle_row(&row_window);
+    new_tile_grid.push(new_tiles);
+
+    removed_papers
+}
+
+fn count_middle_row(row_window: &Vec<Vec<char>>) -> (u32, Vec<char>) {
     let mut accessible_papers: u32 = 0;
 
-
+    let mut new_tiles: Vec<char> = row_window[1].clone();
     for tile_index in 1..row_window[0].len() - 1 {
         // We don't care if empty tiles are accessible (it took me way too long to realize this...)
         let tile = row_window[1][tile_index];
-        if tile == '.' {
+        if tile != '@' {
             continue;
         }
 
@@ -69,8 +95,14 @@ fn count_middle_row(row_window: &Vec<Vec<char>>) -> u32 {
         if window_contents.into_iter().filter(|&c| c == '@').count() < 4 {
             accessible_papers += 1;
             // print!(" => accessible\n")
+            new_tiles[tile_index] = '*';
         }
-
     }
-    accessible_papers
+    (accessible_papers, new_tiles)
+}
+
+fn _display_tile_grid(tile_grid: &Vec<Vec<char>>) {
+    for row in tile_grid.iter() {
+        println!("{}", row.iter().collect::<String>());
+    }
 }
